d7b0ae554499c7caaaf955b28a658a49618fdf51
KSCode <kunalshenoy25@gmail.com>
2025-01-17 11:42:12 -0600

    day20

--- snakeGame/snakeGameDemo.py
+++ snakeGame/snakeGameDemo.py
@@ -1,17 +1,27 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title("Snake Game")
+screen.tracer(0)
 
-starting_positions = [(0,0), (-20,0), (-40,0)]
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.goto(position)
+    snake.move_snake()
+
 
 """
 segment_1 = Turtle("square")
