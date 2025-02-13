9fef79cd3c7e05d0f4926b0461bd509ce2653bb9
KSCode <kunalshenoy25@gmail.com>
2025-02-12 22:54:03 -0600

    day29

--- PasswordManager/password_manager.py
+++ PasswordManager/password_manager.py
@@ -1,6 +1,16 @@
+from tkinter import *
 
-
-
-
+from PasswordManager.main import logo_img
 
 # UI Setup
+
+window = Tk()
+window.title("Password Manager")
+
+canvas = Canvas(height=200, width=200)
+logo1_img = PhotoImage(file="logo.png")
+canvas.create_image(image=logo_img)
+
+
+
+window.mainloop()
