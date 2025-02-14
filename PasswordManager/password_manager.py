e66b2cc044842c119ff87ac6c9eee7ae842aa1cb
KSCode <kunalshenoy25@gmail.com>
2025-02-14 13:20:08 -0600

    day30

--- PasswordManager/password_manager.py
+++ PasswordManager/password_manager.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 # --------- Generate Password --------- #
@@ -34,19 +35,39 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="PLease fill all fields.")
     # message box
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
-                                                      f"\nPassword: {password} \nIs it okay to save?")
+        #is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
+        #                                              f"\nPassword: {password} \nIs it okay to save?")
+        try:
+            with open("data.json", "r") as data_file:
+                # reading old data
+                data = json.load(data_file)
 
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file)
+        else:
+            # updating old data with new data
+            data.update(new_data)
+
+
+            with open("data.json", "w") as data_file:
+                # saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            #data_file.write(f"{website} | {email} | {password}\n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 
