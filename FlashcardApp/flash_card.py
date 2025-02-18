8a4987bd8ae3c52703bdca216ac03ce73a86fb20
KSCode <kunalshenoy25@gmail.com>
2025-02-18 12:03:08 -0600

    day31

--- FlashcardApp/flash_card.py
+++ FlashcardApp/flash_card.py
@@ -1,26 +1,71 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+try:
+
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+# accessing the csv file content to use in the flash cards
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+def remove_current_card():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+
+    # create a new data frame from to_learn list
+    # index=False does not add the index numbers to the newly created list
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
 
 window = Tk()
 window.title("Flash Forge")
 window.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
-canvas.create_text(400, 263, text="word", font=("Ariel", 40, "italic", "bold"), fill="black")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 40, "italic", "bold"), fill="black")
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=cross_image, highlightthickness=0, border=0, activebackground=BACKGROUND_COLOR, width=98, height=97)
+wrong_button = Button(image=cross_image, highlightthickness=0, border=0, activebackground=BACKGROUND_COLOR, width=98, height=97, command=next_card)
 wrong_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-right_button = Button(image=check_image, highlightthickness=0, border=0, activebackground=BACKGROUND_COLOR, width=98, height=98)
+right_button = Button(image=check_image, highlightthickness=0, border=0, activebackground=BACKGROUND_COLOR, width=98, height=98, command=remove_current_card)
 right_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
