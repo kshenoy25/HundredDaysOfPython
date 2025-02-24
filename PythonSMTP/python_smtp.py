51a4021084346372a63ce186c9fb63ccccf848c5
KSCode <kunalshenoy25@gmail.com>
2025-02-24 11:31:31 -0600

    day32

--- PythonSMTP/python_smtp.py
+++ PythonSMTP/python_smtp.py
@@ -1,13 +1,45 @@
+#import smtplib
+
+MY_EMAIL = "hailesbales1244@gmail.com"
+MY_PASSWORD = "ykanesagnzvjfonj"
+#with smtplib.SMTP("smtp.gmail.com") as connection:
+#    connection.starttls() # transport layer security designed to provide communications security over a computer network
+#    connection.login(user=my_email, password=my_password)
+#
+#    connection.sendmail(
+#        from_addr=my_email,
+#        to_addrs="hailesbales1234@yahoo.com",
+#        msg="Subject:Email Test\n\nThis is verfied test from pycharm"
+#    )
+
+
+# current date and time
+#current = dt.datetime.now()
+#year = current.year
+#month = current.month
+#days_of_week = current.weekday()
+
+
+#date_of_birth = dt.datetime(year= 1997, month= 11, day= 25)
+#print(date_of_birth)
+
 import smtplib
+import datetime as dt
+import random
 
-my_email = "hailesbales1244@gmail.com"
-my_password = "ykanesagnzvjfonj"
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls() # transport layer security designed to provide communications security over a computer network
-    connection.login(user=my_email, password=my_password)
+now = dt.datetime.now()
+weekday = now.weekday()
 
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="hailesbales1234@yahoo.com",
-        msg="Subject:Email Test\n\nThis is verfied test from pycharm"
-    )
+if weekday == 1:
+    with open("quotes.txt") as quotes_file:
+        all_quotes = quotes_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Monday Motivation\n\n{quote}")
